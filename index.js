ab6ff60552cadca55b64c41f615d4624f8912ba9
Jovan Damjanovic <jovandamjanovic@gmail.com>
2018-07-25 11:38:17 +0200

    persist card list in mongo

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-var Crawler = require("crawler");
+const Crawler = require("crawler");
 const https = require('https');
+const mongoose = require('mongoose');
+const c = new Crawler();
 
-var c = new Crawler();
-var cardsDb = [];
+mongoose.connect('mongodb://jovandamjanovic:jovan123@ds247001.mlab.com:47001/mtg-spoilers', { useNewUrlParser: true });
+const cardSchema = new mongoose.Schema({
+    name: String,
+    link: String,
+    image: String
+});
+
+const Card = mongoose.model('Card', cardSchema);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('Connected to db!');
+    setInterval(checkCards, 900000);
+});
 
 const checkCards = () => {
     console.log('polling magicspoilers!');
@@ -19,15 +33,45 @@ const checkCards = () => {
                         return {
                             name: $(element).attr('alt'),
                             link: $(element).parent().attr('href'),
-                            image:$(element).attr('src')
+                            image: $(element).attr('src')
                         };
                     })
                     .get();
-                let newCards = cards.filter(c => cardsDb.indexOf(c.name) < 0);
-                if (newCards.length > 0) {
-                    cardsDb = cardsDb.concat(newCards.map(c => c.name));
-                    sendEmailNotification(newCards);
-                }
+                Promise.all(cards.map(async c => {
+                    c.count = await Card.countDocuments({ name: c.name }).exec();
+                    return c;
+                }))
+                .then(countedCards => {
+                        console.info('Checking for cards already in db.')
+                        return countedCards.filter(c => c.count === 0);
+                    })
+                    .then(newCards => {
+                        if (newCards.length > 0) {
+                            console.info('New cards spoiled!');
+                            console.info('Saving into database.');
+                            return newCards.map(c => {
+                                var nc = new Card({
+                                    name: c.name,
+                                    link: c.link,
+                                    image: c.image
+                                });
+                                nc.save();
+                                return c;
+                            });
+                        } else {
+                            console.info('No new cards.');
+                            return newCards;
+                        }
+                    })
+                    .then(cards => {
+                        if(cards.length > 0) {
+                            console.info('Sending email.');
+                            sendEmailNotification(cards);
+                        }
+                    })
+                    .catch((err) => {
+                        console.log(err.message);
+                    })
             }
             done();
         }
@@ -54,5 +98,3 @@ const sendEmailNotification = (cards) => {
     req.write(JSON.stringify(payload));
     req.end();
 }
-
-setInterval(checkCards, 1800000);
