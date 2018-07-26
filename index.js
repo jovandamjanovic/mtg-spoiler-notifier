086c7e548ac1dab2bf76d3d722257c5b3376f4d4
Jovan Damjanovic <jovandamjanovic@gmail.com>
2018-07-26 15:36:21 +0200

    fixing up logs

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const cardSchema = new mongoose.Schema({
     image: String
 });
 
+(function(o){
+    if(o.__ts__){return;}
+    var slice = Array.prototype.slice;
+    ['log', 'debug', 'info', 'warn', 'error'].forEach(function(f){
+      var _= o[f];
+      o[f] = function(){
+        var args = slice.call(arguments);
+        args.unshift(new Date().toISOString());
+        return _.apply(o, args);
+      };
+    });
+    o.__ts__ = true;
+  })(console); //prepends a timestamp to console logs
+
 const Card = mongoose.model('Card', cardSchema);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
